e510335ac3a49f22c27f0372547312c3df1a64b5
Phan Quoc Hung <94614398+Healer26-06-01@users.noreply.github.com>
2023-10-20 19:14:32 +0700

    Thiết kế web

---
+++ script/javascripts.js
@@ -0,0 +1,30 @@
+﻿function checkDN() {
+    var tk = document.getElementById("tk").value;
+    if (tk == "") {
+        document.getElementById("hienLoi").style.display = 'block';
+        document.getElementById("hienLoi").innerHTML = "Error: Yêu cầu tên tài khoản.";
+
+    } else {
+        document.getElementById("hienLoi").style.display = 'block';
+        document.getElementById("hienLoi").innerHTML = "Lỗi: Xin điền mật khẩu.";
+    }
+}
+
+function checkDK() {
+    var email = document.getElementById("email").value;
+    var mk = document.getElementById("mk").value;
+    var nl_mk = document.getElementById("nl_mk").value;
+    if (email == "") {
+        document.getElementById("hienLoi").style.display = 'block';
+        document.getElementById("hienLoi").innerHTML = "Error: Vui lòng cung cấp địa chỉ email hợp lệ.";
+
+    } else {
+        document.getElementById("hienLoi").style.display = 'block';
+        document.getElementById("hienLoi").innerHTML = "Error: Vui lòng nhập mật khẩu tài khoản.";
+    }
+
+    if (mk != nl_mk) {
+        document.getElementById("hienLoi").style.display = 'block';
+        document.getElementById("hienLoi").innerHTML = "Error: Mật khẩu không khớp.";
+    }
+}
